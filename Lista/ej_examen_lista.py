20aeed6fbe491ba6e7684acda0023fbcf5225c49
GonzaloCepeda9 <95876460+GonzaloCepeda9@users.noreply.github.com>
2026-08-25 18:35:19 -0300

    Ejercicio tipo parcial  de Lista

---
+++ Lista/ej_examen_lista.py
@@ -0,0 +1,14 @@
+# Ejercicio tipo parcial:
+'''
+Dada una lista de personajes de marvel (usar el archivo adjunto) debe tener 100 o mas, resolver:
+    a. Listado ordenado de manera ascendente por nombre de los personajes.
+    b. Determinar en que posicion esta The Thing y Rocket Raccoon.
+    c. Listar todos los villanos de la lista.
+    d. Poner todos los villanos en una cola para determinar luego cuales aparecieron antes de 1980.
+    e. Listar los superheores que comienzan con  Bl, G, My, y W.
+    f. Listado de personajes ordenado por nombre real de manera ascendente de los personajes.
+    g. Listado de superheroes ordenados por fecha de aparación.
+    h. Modificar el nombre real de Ant Man a Scott Lang.
+    i. Mostrar los personajes que en su biografia incluyan la palabra time-traveling o suit.
+    j. Eliminar a Electro y Baron Zemo de la lista y mostrar su información si estaba en la lista.
+'''
