c7c379f88e9889d999eb2d905896421a22c7be77
GonzaloCepeda9 <95876460+GonzaloCepeda9@users.noreply.github.com>
2026-08-25 18:35:59 -0300

    Actualización de código de búsqueda binaria

--- Recursividad/ej_extra_busqueda_binaria.py
+++ Recursividad/ej_extra_busqueda_binaria.py
@@ -1,24 +1,27 @@
+from typing import Any
+
 # Extra: Implementar una función de búsqueda que devuelva la posición de un número en una lista.
 
-numbers = [0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99]
-
-def busqueda_binaria_recursiva(array, valor_busc, primero = 0, ultimo = len(numbers)) -> int: 
+# Búsqueda binaria recursiva
+def busqueda_binaria_recursiva(array, valor_busc, primero = 0, ultimo = None) -> int: 
+    if ultimo is None:
+        ultimo = len(array) - 1
     medio = (primero + ultimo) // 2
-    if primero >= ultimo:
+    if primero > ultimo:
         return None
     elif array[medio] == valor_busc:
         return medio
     else:
         if array[medio] > valor_busc:
-            return busqueda_binaria_recursiva(array, valor_busc, primero, ultimo-1)
+            return busqueda_binaria_recursiva(array, valor_busc, primero, medio-1)
         else:
-            return busqueda_binaria_recursiva(array, valor_busc, primero+1, ultimo)
+            return busqueda_binaria_recursiva(array, valor_busc, medio+1, ultimo)
 
-def busqueda_binaria_iterativa(array, valor_buscado):
+# Búsqueda binaria iterativa
+def busqueda_binaria_iterativa(array: list, valor_buscado: Any) -> int:
     primero = 0
     ultimo = len(array) -1
     posicion = -1
-
     while primero <= ultimo and posicion == -1:
         medio = (primero + ultimo) // 2 
         if array[medio] == valor_buscado:
@@ -32,16 +35,20 @@ def busqueda_binaria_iterativa(array, valor_buscado):
     else:
         return medio
 
+# --------- Carga de datos ---------
+numbers = [0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90, 99]
+
 #################################################  Ejecución de pruebas del enunciado  #################################################
-print('\n-------------------------------------- Búsqueda de un número entero dentro de una lista --------------------------------------')
-buscado = 9
+print('\n----------------------------- Búsqueda binaria recursiva de un número entero dentro de una lista -----------------------------')
+buscado = 0
 posicion = busqueda_binaria_recursiva(numbers, buscado)
 print(f'Búsqueda binaria recursiva:')
-if posicion:
+if posicion is not None:
     print(f'El número {buscado} se encuentra en la posición {posicion}.')
 else:
     print(f'El número {buscado} no se encuentra en la lista.')
 
+print('\n----------------------------- Búsqueda binaria iterativa de un número entero dentro de una lista -----------------------------')
 buscado = 18
 posicion = busqueda_binaria_iterativa(numbers, buscado)
 print(f'\nBúsqueda binaria iterativa:')
